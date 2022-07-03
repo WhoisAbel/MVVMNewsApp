463345cccfcbf4c22448676a1ba41558484268b3
WhoisAbel <whoisabel1996@gmail.com>
2022-07-03 12:25:49 +0430

    Notice about private key.

--- app/src/main/java/com/example/mvvmNewsApp/util/Constants.kt
+++ app/src/main/java/com/example/mvvmNewsApp/util/Constants.kt
@@ -2,6 +2,7 @@ package com.example.mvvmNewsApp.util
 
 class Constants {
     companion object{
+        // We must not share this API_KEY on git
         const val API_KEY="411f8f0a00a444229f7f50a74874ce5a"
         const val BASE_URL="https://newsapi.org/"
     }
